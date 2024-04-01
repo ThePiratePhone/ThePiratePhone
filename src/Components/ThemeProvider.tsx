36ca76444c78ffff89b1fc61f2cc142d3dc36423
Wiwok <emil.dechenaud@gmail.com>
2024-04-01 17:50:10 +0200

    Added petrograd theme

--- src/Components/ThemeProvider.tsx
+++ src/Components/ThemeProvider.tsx
@@ -1,6 +1,7 @@
+import auroraurban from '../Themes/auroraurban';
 import blackandwhite from '../Themes/blackandwhite';
 import deepocean from '../Themes/deepocean';
-import auroraurban from '../Themes/auroraurban';
+import petrogradsun from '../Themes/petrogradsun';
 import radiantreef from '../Themes/radiantreef';
 
 const Themes = new Map<string, Theme>();
@@ -9,6 +10,7 @@ Themes.set('default', { CSS: {}, name: 'Défaut' });
 Themes.set('radiantreef', { CSS: radiantreef, name: 'Récif radieux' });
 Themes.set('deepocean', { CSS: deepocean, name: 'Océan profond' });
 Themes.set('auroraurban', { CSS: auroraurban, name: 'Aurore urbaine' });
+Themes.set('petrogradsun', { CSS: petrogradsun, name: 'Soleil de Petrograd ☭' });
 Themes.set('blackandwhite', { CSS: blackandwhite, name: 'Noir et blanc' });
 
 function getThemes() {
@@ -28,4 +30,4 @@ function ThemeProvider({ themeId, children }: { themeId: string; children: JSX.E
 }
 
 export default ThemeProvider;
-export { getThemes, getTheme };
+export { getTheme, getThemes };
---
+++ src/Themes/petrogradsun.ts
@@ -0,0 +1,30 @@
+export default {
+	'--color1v1': '#eb4848',
+	'--color1v2': '#cf2626',
+	'--color1v1Border': '#cf2626',
+	'--color1v2Border': '#b52020',
+	'--color1Text': 'white',
+
+	'--color2v1': '#ba8a12',
+	'--color2v2': '#966512',
+	'--color2v1Border': '#966512',
+	'--color2v2Border': '#7c5017',
+	'--color2Text': 'white',
+
+	'--inputColor': '#cdabab',
+	'--inputText': 'black',
+
+	'--disabledButton': '#ced1d3',
+	'--disabledButtonBorder': '#a9aeb1',
+	'--disabledButtonText': 'black',
+
+	'--footerbg': 'black',
+	'--footerText': 'white',
+
+	'--headerColor': '#3f3c3c',
+	'--headerText': 'white',
+	'--navButtonBorder': 'white',
+
+	'--backgroundColor': '#282525',
+	'--backgroundColorText': 'white'
+} as React.CSSProperties;
